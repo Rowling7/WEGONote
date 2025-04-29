e254233d4678c3be2ee2d2ac687c21dbc1fe86da
D.4M._YS_GH <2804119807@qq.com>
2025-04-29 08:29:41 +0800

    更新日志

--- 日志/每月脚本执行情况/DRG运维-CYS25年3月留底-学习.sql
+++ 日志/每月脚本执行情况/DRG运维-CYS25年3月留底-学习.sql
@@ -1,30 +1,30 @@
  ----查询DRG结算的上报失败的需处理问题的清单
- select mdtrt_sn ,err_msg   
- from t_setlinfo a 
- left join t_mihs_result_relation b on a.mdtrt_sn = b.uid 
+ select mdtrt_sn ,err_msg
+ from t_setlinfo a
+ left join t_mihs_result_relation b on a.mdtrt_sn = b.uid
  left join t_mihs_result c on b.resultid=c.id where c.infocode=-1
  and c.err_msg  not like '%省平台%'-- and c.err_msg   like '%checkResult：OPER_DR_NAME：必填项校验错误%'
- and a.brjsrq >='2025-03-01' and a.brjsrq<'2025-04-01'   -- and  isdrg=1  
+ and a.brjsrq >='2025-03-01' and a.brjsrq<'2025-04-01'   -- and  isdrg=1
 
  --select * from t_setlinfo_oprninfo where  mdtrt_sn='1058796-8-001' order by oprn_oprt_code
 
- select * from t_setlinfo_oprninfo where mdtrt_sn in 
+ select * from t_setlinfo_oprninfo where mdtrt_sn in
 
- select wm_dise_code from t_setlinfo where mdtrt_sn='2023368218-7-001' 
+ select wm_dise_code from t_setlinfo where mdtrt_sn='2023368218-7-001'
 
   ----查询非DRG结算且需要上传的，但上报失败的需处理的清单
- select mdtrt_sn ,err_msg   
- from t_setlinfo a 
- left join t_mihs_result_relation b on a.mdtrt_sn = b.uid 
+ select mdtrt_sn ,err_msg
+ from t_setlinfo a
+ left join t_mihs_result_relation b on a.mdtrt_sn = b.uid
  left join t_mihs_result c on b.resultid=c.id where c.infocode=-1
- and c.err_msg  not like '%省平台%' 
+ and c.err_msg  not like '%省平台%'
  and a.brjsrq >='2025-02-01' and a.brjsrq<'2025-03-01'    and  isdrg=0
  and a.mdtrt_sn in ( select  zylsh from  t_job_settlebillinglist   )
 
 
   --手术重复处理--时间重复
   update t_setlinfo_oprninfo set  oprn_oprt_date=dateadd(MINUTE,1,oprn_oprt_date)  where  id in (select  id from  (select   id,ROW_NUMBER() over(partition by mdtrt_sn,oprn_oprt_code,oprn_oprt_date   order by id) rn  from(select a.* from t_setlinfo_oprninfo a,(select mdtrt_sn,oprn_oprt_code,oprn_oprt_date,count(*) cont  from  t_setlinfo_oprninfo  group by  mdtrt_sn,oprn_oprt_code,oprn_oprt_date  having count(*)>1 ) b  where a.mdtrt_sn=b.mdtrt_sn and a.oprn_oprt_code=b.oprn_oprt_code  and a.oprn_oprt_date=b.oprn_oprt_date
- and a.mdtrt_sn in ( select mdtrt_sn  from t_setlinfo a  left join t_mihs_result_relation b on a.mdtrt_sn = b.uid 
+ and a.mdtrt_sn in ( select mdtrt_sn  from t_setlinfo a  left join t_mihs_result_relation b on a.mdtrt_sn = b.uid
  left join t_mihs_result c on b.resultid=c.id where c.infocode=-1 and c.err_msg not like '%省平台%' And c.err_msg   like  '%JSQD_OPERATION_INFO%' ) ) a   ) id    where rn=1  )
 
  select  * from t_mihs_result where uid='IP0000244303-001'
@@ -35,14 +35,14 @@
 
  -----
  delete from t_mihs_result_relation where uid in
- (select mdtrt_sn from t_setlinfo a 
- left join t_mihs_result_relation b on a.mdtrt_sn = b.uid 
+ (select mdtrt_sn from t_setlinfo a
+ left join t_mihs_result_relation b on a.mdtrt_sn = b.uid
  left join t_mihs_result c on b.resultid=c.id where c.infocode=-1
  and c.err_msg  not like '%省平台%'  and c.err_msg like '%违反唯一约束条件(MIDR.OPERATION_UNIQUE_M)%'
  and a.brjsrq >='2024-11-01' and a.brjsrq<'2024-12-01'    and  isdrg=1)   --2022745135-1-001  11月25日
 
  -----更新手术医生标准码
- update t_setlinfo_oprninfo set oper_dr_code_std='D371072007682' where id in 
+ update t_setlinfo_oprninfo set oper_dr_code_std='D371072007682' where id in
  (select id from t_setlinfo_oprninfo where  oper_dr_name='于海英'  and oper_dr_code='12140'and  (oper_dr_code_std is null or oper_dr_code_std not like 'D%') and oprn_oprt_date>'2024-10-31'and oprn_oprt_date<='2024-11-30')
       --处理完了用下面脚本查下还有没有其他手术医生存在问题，有的话参照上面处理下
  select * from t_setlinfo_oprninfo where   (oper_dr_code_std is null or oper_dr_code_std not like 'D%') and oprn_oprt_date>'2024-10-31'and oprn_oprt_date<='2024-11-30'
@@ -60,10 +60,10 @@ UPDATE t_setlinfo SET adm_caty='A50',dscg_caty='A50' where id='755724'
 
 ---------更新结算清单患者基本信息表的 bill_code, bill_no
    select bill_code from t_setlinfo where mdtrt_sn='2023331038-4-001' ;
-   select bill_code from t_setlinfo where mdtrt_sn='2023787144-1-001' 
+   select bill_code from t_setlinfo where mdtrt_sn='2023787144-1-001'
 
       select * from t_setlinfo where mdtrt_sn='428005-19-001' ;
-   select * from t_setlinfo where mdtrt_sn='1046940-9-003' 
+   select * from t_setlinfo where mdtrt_sn='1046940-9-003'
    --id 478972  ---1046940-9-003  1001016814  --240126834-1-001--1001016814
    update t_setlinfo set bill_code='1000997166', bill_no='1000997166' where id='486558'    --2023331038-4-001
 
@@ -106,8 +106,8 @@ checkResult：DIAG_CODE：必填项校验错误；DIAG_NAME：必填项校验错误；；
 
 select * from t_setlinfo_diseinfo where mdtrt_sn='240001113-7-001'
 
-select * from t_job_settlebillinglist where datebill>='20241101' and datebill<'20241231' and medicalhosid=1 and zylsh='1085565-4-001'  group by zylsh; 
-------------对数----- 
+select * from t_job_settlebillinglist where datebill>='20241101' and datebill<'20241231' and medicalhosid=1 and zylsh='1085565-4-001'  group by zylsh;
+------------对数-----
 --1\--select  distinct  zylsh from t_job_settlebillinglist where convert(date,datebill,23)>='2025-02-01' and convert(date,datebill,23)<'2025-03-01' and medicalhosid=1  ;
 --2\--update t_setlinfo set isdrg=0 where convert(date,brjsrq,23)>='2025-02-01' and convert(date,brjsrq,23)<'2025-03-01';
 --3\--update t_setlinfo set isdrg=1 where mdtrt_sn in (select zylsh from t_job_settlebillinglist where convert(date,datebill,23)>='2025-02-01' and convert(date,datebill,23)<'2025-03-01' and medicalhosid=1 group by zylsh);
@@ -115,7 +115,7 @@ select * from t_job_settlebillinglist where datebill>='20241101' and datebill<'2
 --5\--update t_setlinfo set hi_type='390' where mdtrt_sn in (select zylsh from t_job_settlebillinglist where convert(date,datebill,23)>='2025-02-01' and convert(date,datebill,23)<'2025-03-01'and medicalhosid=1 and insuranceid='390' group by zylsh);
 --6\--delete t_drg_result_relation where uid in (select mdtrt_sn from t_setlinfo a left join t_drg_result_relation b on a.mdtrt_sn=b.uid left join t_drg_result c on b.drgresult_id=c.id where a.hi_type <> c.insurtype)
 
-select case when hi_type=310 then '职工' when hi_type=390 then '居民' end 类别,count(*)数量 
+select case when hi_type=310 then '职工' when hi_type=390 then '居民' end 类别,count(*)数量
 from t_setlinfo where BRJSRQ>='2025-02-01' and BRJSRQ<'2025-03-01' and isdrg=1 group by hi_type
 
 
@@ -142,7 +142,7 @@ from t_setlinfo where BRJSRQ>='2025-02-01' and BRJSRQ<'2025-03-01' and isdrg=1 g
 --646877-18-001,804417-5-001 更新手术日期脚本后重新上传
 
 ---2025-02-24
---2022802800-1-008 更新手术日期脚本无果，修订后上传;     
+--2022802800-1-008 更新手术日期脚本无果，修订后上传;
 
 --select * from t_setlinfo_diseinfo where mdtrt_sn='992849-14-002' order by diag_code --MIDR.OUT_UNIQUE_M 提示诊断重复，数据库表没有重复，修订中删除重复解决
 
@@ -175,9 +175,9 @@ select * from t_setlinfo_diseinfo where mdtrt_sn='660584-2-001'
 select * from t_setlinfo_diseinfo where diag_code='M20.100x002'  --后天性(足母)外翻 9A0EE037-A969-467C-BC0F-5F6D0EE9DC9A
 update t_setlinfo_diseinfo set diag_name='后天性(足母)外翻' where id='9A0EE037-A969-467C-BC0F-5F6D0EE9DC9A'
 
-select * from t_setlinfo_diseinfo where diag_code='T31.300' 
+select * from t_setlinfo_diseinfo where diag_code='T31.300'
 
---2022901628-1-001  448211-7-001 2022960598-4-001 2023690968-1-001  单抽过来上传1053410-16-001    2023713619-1-001   250016529-1-002  
+--2022901628-1-001  448211-7-001 2022960598-4-001 2023690968-1-001  单抽过来上传1053410-16-001    2023713619-1-001   250016529-1-002
 
 
 -----2024-03-11
